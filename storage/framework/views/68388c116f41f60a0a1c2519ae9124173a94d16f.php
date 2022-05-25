f8fdb50a6bd43268adbacf176f895bd249348072
Raphaeasar <37519315+Raphaeasar@users.noreply.github.com>
2022-05-25 19:48:59 -0300

    Delete storage directory

--- storage/framework/views/68388c116f41f60a0a1c2519ae9124173a94d16f.php
+++
@@ -1,5 +0,0 @@
-
-<?php $__env->startSection("conteudo"); ?>
-    <?php echo $__env->make("_produtos", [ 'lista' => $lista ], \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make("layout", \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\lara-teste\resources\views/home.blade.php ENDPATH**/ ?>
